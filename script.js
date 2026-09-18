911e21a563f938dcad3602203e5246b407bb75ac
The-Solarity <pnkhang9500@proton.me>
2026-09-18 16:22:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,19 @@ dot.addEventListener("click", function () {
 });
 
 const loadingScreen = document.getElementById("loading-screen");
+const continueText = document.getElementById("continue-text");
 const music = document.getElementById("background-music");
 
+let ready = false;
+
+setTimeout(() => {
+    ready = true;
+    loadingScreen.classList.add("loading-screen-ready");
+}, 2500);
+
 function enterSite() {
+    if (!ready) return;
+
     music.play();
     loadingScreen.classList.add("hidden");
 
