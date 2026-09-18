6cb903d0b110c5408d9c8d8f8992d71aa041a567
The-Solarity <pnkhang9500@proton.me>
2026-09-18 13:53:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const discord = document.getElementById("discord-copy");
+const discordIcon = discord.querySelector("img");
 
 discord.addEventListener("click", async function (event) {
     event.preventDefault();
@@ -7,9 +8,18 @@ discord.addEventListener("click", async function (event) {
         await navigator.clipboard.writeText("the_lunarity");
 
         discord.classList.add("copied");
+        discordIcon.style.display = "none";
+
+        const text = document.createElement("span");
+        text.textContent = "Copied!";
+        text.classList.add("copy-text");
+
+        discord.appendChild(text);
 
         setTimeout(() => {
             discord.classList.remove("copied");
+            text.remove();
+            discordIcon.style.display = "";
         }, 1000);
 
     } catch (error) {
