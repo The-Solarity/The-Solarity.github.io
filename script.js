dfa70b891d19021dd9937c88a6d8d8702c5b364b
The-Solarity <pnkhang9500@proton.me>
2026-09-18 13:51:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,6 @@ discord.addEventListener("click", async function (event) {
 
     try {
         await navigator.clipboard.writeText("the_lunarity");
-        console.log("Discord name copied!");
-    } catch (error) {
-        console.error("Clipboard failed:", error);
-    }
-});
 
         discord.classList.add("copied");
 
