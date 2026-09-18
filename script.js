cd7e28f59ebb2062094cf84eeff1ee12a646755a
The-Solarity <pnkhang9500@proton.me>
2026-09-18 16:20:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const message = document.getElementById("hidden-message");
 dot.addEventListener("click", function () {
     message.textContent = "You found something.";
 });
+
+const loadingScreen = document.getElementById("loading-screen");
+const music = document.getElementById("background-music");
+
+function enterSite() {
+    music.play();
+    loadingScreen.classList.add("hidden");
+
+    document.removeEventListener("keydown", enterSite);
+}
+
+loadingScreen.addEventListener("click", enterSite);
+document.addEventListener("keydown", enterSite);
